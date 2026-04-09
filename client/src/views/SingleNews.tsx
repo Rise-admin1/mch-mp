669a2ab1e44377966ce09bbd93791c4f30bdc5aa
aju-alen <alenaju1304@gmail.com>
2026-04-09 09:20:04 +0400

    url share

--- client/src/views/SingleNews.tsx
+++ client/src/views/SingleNews.tsx
@@ -39,7 +39,7 @@ const SingleNews: React.FC<{ id: string }> = ({ id }) => {
         setIsModalOpen(false);
     };
 
-    const shareUrl = `${getPublicSiteUrl()}/news/${id}`;
+    const shareUrl = `https://www.funyula.com/news/${id}`;
     const shareTitle = newsItem ? newsItem.title : 'News Article';
     const shareDescription = newsItem ? extractDescriptionFromContent(newsItem.content) : '';
 
